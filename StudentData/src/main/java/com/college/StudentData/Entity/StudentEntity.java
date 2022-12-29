aa555c1505051c14ea79d044fb6b54093387f059
arvind019 <arvindpatidar019@gmail.com>
2022-12-29 19:56:47 +0530

    Student Info Application

---
+++ StudentData/src/main/java/com/college/StudentData/Entity/StudentEntity.java
@@ -0,0 +1,41 @@
+package com.college.StudentData.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "STUDENTS_DTL")
+public class StudentEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue
+    private Integer id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "COURSE")
+    private String course;
+    @Column(name = "YEAR")
+    private Integer year;
+    @Column(name = "BRANCH")
+    private String branch;
+    @Column(name = "MOBILE")
+    private String mobile;
+    @Column(name = "GRADUATION_YEAR")
+    private Integer graduationYear;
+}
+
+
+
+
+
+
+
